c3b6236529871de1bf3956e48cda5fe83404368d
Aaron Weissman <weissman.aaron@gmail.com>
2018-11-29 11:18:01 -0700

    knex and heroku

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,14 @@ module.exports = {
   seeds: {
     directory: './db/seeds/dev',
   },
-  useNullAsDefault: true
+  useNullAsDefault: true,
+  production: {
+    client: 'pg',
+    connection: process.env.postgres://abokzeobtxmdyx:66c4a49a12b93889201062c278f7e7d902db9ba02445214bfb985dae1eab2b29@ec2-54-235-133-42.compute-1.amazonaws.com:5432/d707m4p7cnm1rg + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 
 };
